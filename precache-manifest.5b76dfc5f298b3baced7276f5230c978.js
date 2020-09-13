3e42af4a0e4335dba3c9a6f340cb0d173ab16026
Oscar Javier Ramirez Torres <ojramirez@bancatlan.hn>
2020-09-13 16:43:18 -0600

    Updates

---
+++ precache-manifest.5b76dfc5f298b3baced7276f5230c978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f5e51753d64068f8469e3603d11a0c",
+    "url": "/my-task-list/index.html"
+  },
+  {
+    "revision": "1cda0c25cde220e37360",
+    "url": "/my-task-list/static/css/main.9c75ff92.chunk.css"
+  },
+  {
+    "revision": "44099cb3bbef77a5c83d",
+    "url": "/my-task-list/static/js/2.21dae745.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/my-task-list/static/js/2.21dae745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cda0c25cde220e37360",
+    "url": "/my-task-list/static/js/main.ca14df23.chunk.js"
+  },
+  {
+    "revision": "adc7460bb36cf49ef9c8",
+    "url": "/my-task-list/static/js/runtime-main.3cc73607.js"
+  }
+]);
